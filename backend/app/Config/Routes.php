1ac3bba270dfbe4421bd1f6f729b832f108da937
hoshewashitape <167269660+hoshewasheetape1018@users.noreply.github.com>
2025-09-22 08:54:33 +0000

    refactor(front-end): change landing page route name

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,4 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/skibidi', 'Users::index');
+$routes->get('/home', 'Users::index');
