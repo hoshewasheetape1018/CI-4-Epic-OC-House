34e7a49ff1c0c154e94a2ad4f617b4a59879a91d
hoshewashitape <167269660+hoshewasheetape1018@users.noreply.github.com>
2025-09-29 00:40:58 +0000

    feat(front-end) moodboard news roadmap

---
+++ backend/app/Views/user/roadmap.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?= view('components/head') ?>
+    
+<body>
+
+    <!-- header -->
+    <?= view('components/header') ?>
+
+    <!-- start of roadmap page -->
+
+    <section>
+    roadmap page
+    </section>
+
+    </body>
+
+    <!-- footer -->
+    <?= view('components/footer') ?>
+
+</html>
